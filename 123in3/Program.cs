287a747ace66edff80a1374ef14bed5287bc4a44
Yuliya <darkwader@mail.ru>
2023-02-12 21:43:35 +0300

    123in3

---
+++ 123in3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 100)
+if (n < 1000)
+{
+int n1 = n % 10; // 586=58
+Console.WriteLine($"Число: {n1}");
+}
+
+if (n < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
+if (n >= 1000)
+if (n < 100000)
+{
+int n1 = n / 100 % 10; 
+Console.WriteLine($"Число: {n1}");
+}
